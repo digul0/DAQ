166c681db1863d29404bcf8dda42ed29512f80e6
digul0 <digul@yandex.ru>
2018-12-25 11:32:32 +0300

    Replace most part of implementations from .h to .cpp

--- m_log.cpp
+++ m_log.cpp
@@ -3,9 +3,9 @@
 #include <sstream>
 
 const std::string m_log::make_name_from_time(const std::string name_prefix,
-                                       std::chrono::system_clock::time_point tp,
-                                       const std::string name_postfix
-                                      )
+        std::chrono::system_clock::time_point tp,
+        const std::string name_postfix
+                                            )
 {
     auto current_time_t        = std::chrono::system_clock::to_time_t(tp);
     auto current_local_time    (std::localtime(&current_time_t));
@@ -15,3 +15,58 @@ const std::string m_log::make_name_from_time(const std::string name_prefix,
     const std::string log_name = name_prefix + os.str() + name_postfix;
     return log_name;
 }
+m_log::m_log(std::ostream& out_dest): out_(&out_dest)
+{
+
+}
+m_log::m_log(std::ofstream& out_file): out_(&out_file)
+{
+
+}
+m_log::m_log(m_log::other)
+{
+    if (p_setted_file_!=nullptr)
+        {
+            out_ = p_setted_file_;
+        }
+    else
+        throw std::logic_error("out log file not set!");
+}
+m_log::m_log(m_log&& rlog) noexcept:
+    out_ ( rlog.out_), ss_(std::move(rlog.ss_))
+{
+
+}
+m_log::~m_log()
+{
+    if (out_!=nullptr)
+        write_to(*out_);
+}
+void m_log::setLogfile(std::ofstream* out_file)
+{
+    p_setted_file_ = out_file;
+}
+template<typename T>
+void m_log::write_to(T&)
+{
+    auto& relevant_mutex = Mutex_selector<T>::get();
+    std::lock_guard<std::mutex> ul(relevant_mutex);
+    *out_ << ss_.rdbuf() << std::flush;
+}
+
+template <typename T>
+std::mutex& Mutex_selector<T>::get()
+    {
+        if (std::is_same_v<std::ofstream,T> )
+            {
+                return mut_file_;
+            }
+        else if (std::is_same_v<std::ostream,T>)
+            {
+                return mut_console_;
+            }
+        else
+            {
+                return any_common_mut_;
+            }
+    };
--- m_log.h
+++ m_log.h
@@ -12,6 +12,7 @@ class Mutex_selector;
 /**
 * @brief
 * Provides multi-threaded console and file output.
+* @warning Do not use endl, ends. Instead of ones use '\n'
 */
 class m_log
 {
@@ -20,53 +21,31 @@ public:
     enum class other { to_setted_log_file };
 
     // for stdout
-    explicit m_log(std::ostream& out_dest = std::cout ): out_(&out_dest)
-    {
+    explicit m_log(std::ostream& out_dest = std::cout );
 
-    }
     // for file
-    explicit m_log(std::ofstream& out_file ): out_(&out_file)
-    {
+    explicit m_log(std::ofstream& out_file );
 
-    }
     //for setted file
-    explicit m_log(m_log::other)
-    {
-        if (p_setted_file_!=nullptr)
-            {
-                out_ = p_setted_file_;
-            }
-        else
-            throw std::logic_error("out log file not set!");
-    }
-    m_log(m_log&& rlog) noexcept:
-        out_ ( rlog.out_), ss_(std::move(rlog.ss_))
-    {
+    explicit m_log(m_log::other);
 
-    }
+    m_log(m_log&& rlog) noexcept;
+
     m_log(const m_log&)            = delete;
+
     m_log& operator=(const m_log&) = delete;
-    ~m_log()
-    {
-        if (out_!=nullptr)
-            write_to(*out_);
-    }
-    void static setLogfile(std::ofstream* out_file)
-    {
-        p_setted_file_ = out_file;
-    }
+
+    //output perform in destructor.
+    ~m_log();
+
+    void static setLogfile(std::ofstream* out_file);
 private:
     template<typename T>
-    void write_to(T&)
-    {
-        auto& relevant_mutex = Mutex_selector<T>::get();
-        std::lock_guard<std::mutex> ul(relevant_mutex);
-        *out_ << ss_.rdbuf() << std::flush;
-    }
-    template< typename T>
-    friend m_log&&  operator<<(m_log&&  m_log_, const T& t) noexcept;
+    void write_to(T&);
 
-    //inline static std::mutex mut_console_, mut_file_;
+    template< typename T>
+    friend m_log&&  operator<<(m_log&&  m_log_, T&& t) noexcept;
+
     std::ostream* out_ {nullptr};
     std::stringstream ss_;
     inline static std::ofstream* p_setted_file_ {nullptr};
@@ -77,38 +56,24 @@ public:
                          const std::string name_postfix);
 };
 
-//Atomic output for <<  until m_log object is live
-template< typename T>
-m_log&& operator<<(m_log&& m_log_, const T& t ) noexcept
-{
-    m_log_.ss_ << t;
-    return std::move(m_log_);
-}
-
 //choose relevant static mutex
 template<typename T>
 class Mutex_selector
 {
 public:
-    static std::mutex& get()
-    {
-        if (std::is_same_v<std::ofstream,T> )
-            {
-                return mut_file_;
-            }
-        else if (std::is_same_v<std::ostream,T>)
-            {
-                return mut_console_;
-            }
-        else
-            {
-                return any_common_mut_;
-            }
-    };
+    static std::mutex& get();
 private:
     inline static std::mutex mut_console_,
            mut_file_,
            any_common_mut_;
+};
+
+//Atomic output for <<  until m_log object is live
+template< typename T>
+static m_log&& operator<<(m_log&& m_log_, T&& t ) noexcept
+{
+    m_log_.ss_ << std::forward<decltype(t)>(t);
+    return std::move(m_log_);
 };
 #endif // M_LOG_H
 
